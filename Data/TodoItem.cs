70860adcb3addb84786d22d1e94aab5052e8e367
nihalozbozduman <ozbozduman.nihal@gmail.com>
2025-12-03 03:15:20 +0300

    Update TodoItem.cs

--- Data/TodoItem.cs
+++ Data/TodoItem.cs
@@ -1,13 +1,22 @@
+using SQLite;
+
 namespace TodoApp_Nihal.Data;
 
+[Table("TodoItems")]
 public class TodoItem
 {
+    [PrimaryKey, AutoIncrement]
     public int Id { get; set; }
-    public string? Title { get; set; }
-    public DateTime DueDate { get; set; }
-    public DateTime CreatedDate { get; set; }
-    public bool IsComplete { get; set; }
 
+    public string? Title { get; set; }
+
+    public DateTime DueDate { get; set; }
+
+    public DateTime CreatedDate { get; set; }
+
+    public bool IsComplete { get; set; }
+    
+    [Ignore] 
     public bool IsOverdue => !IsComplete && DueDate.Date < DateTime.Today;
 
     public TodoItem()
